cfb96a002ce9b6eebdb71a8ed2c9472136f82093
teguz2000 <189381830+teguz2000@users.noreply.github.com>
2024-12-15 06:40:07 +0100

    Done 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
 
+    let result = "";
+
+    if(Number(number) === false || number < 0)
+    {
+        result = "ERROR";
+        return result;
+        
+    }
+
+    for(let i = 0; i < number; i++)
+    {
+        result += string;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
