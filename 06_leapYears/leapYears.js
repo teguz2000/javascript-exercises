c4d6776ea2289096d32a5475d67a256610f49c30
teguz2000 <189381830+teguz2000@users.noreply.github.com>
2024-12-15 17:31:08 +0100

    Done 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    let result;
+
+    (year % 400 === 0) 
+        ? result = true 
+        : (year % 100 === 0) 
+            ? result = false
+            : (year % 4 === 0)
+                ? result = true
+                : result =false;
+
+    return result;
 };
 
 // Do not edit below this line
