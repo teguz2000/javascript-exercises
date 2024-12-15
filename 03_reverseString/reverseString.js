452e8127cc832a69638c2cd64f36f93d1436294b
teguz2000 <189381830+teguz2000@users.noreply.github.com>
2024-12-15 07:13:30 +0100

    Done 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+
+const reverseString = function(string) {
+
+    let result = "";
+
+    for(let i = string.length - 1; i >= 0; i--)
+    {
+        result += string.charAt(i);
+    }
+
+    return result;
 
 };
 
