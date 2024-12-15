1fc4c2144cb2ae9b32eeabd208ffb18e511a2838
teguz2000 <189381830+teguz2000@users.noreply.github.com>
2024-12-15 11:11:06 +0100

    Done 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array = [], ...toCut) {
+
+    let result = [];
+
+    for(let i = 0; i < array.length; i++)
+    {
+        for(let j = 0; j < toCut.length; j++)
+        {
+            if(array[i] === toCut[j])
+            {
+                array.splice(i,1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
