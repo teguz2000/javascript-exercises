56b33786f2386f0541b8ffbf80d9acd6eeba56e2
teguz2000 <189381830+teguz2000@users.noreply.github.com>
2024-12-15 11:51:49 +0100

    Done 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
 
+    if(
+        typeof(number1 + number2) === "string" ||
+        (number1 < 0) === true ||
+        (number2 < 0) === true ||
+        !Number.isInteger(number1 + number2)
+    )
+    {
+        return "ERROR";
+    }
+
+    let result = 0;
+
+    for(let i = Math.min(number1, number2); i <= Math.max(number1,number2); i++)
+    {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
