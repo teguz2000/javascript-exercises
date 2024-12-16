69c40e0d53d53e54f350b1e6bfccf63a1debdd1e
teguz2000 <189381830+teguz2000@users.noreply.github.com>
2024-12-16 05:00:21 +0100

    Done 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
+const add = function(number1, number2) {
 	
+  return number1 + number2;
 };
 
-const subtract = function() {
+const subtract = function(number1, number2) {
 	
+  return number1 - number2;
 };
 
-const sum = function() {
+const sum = function(numbers = []) {
 	
+  let result = 0;
+
+  for(number of numbers)
+    result += number;
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(numbers = []) {
 
+  let result = 1;
+
+  for(let number of numbers)
+    result *= number;
+
+  return result;
 };
 
-const power = function() {
+const power = function(number1, number2) {
 	
+  return number1 ** number2;
 };
 
-const factorial = function() {
+const factorial = function(num) {
 	
+  if (num < 0) 
+      return -1;
+  else if (num == 0) 
+    return 1;
+  else {
+    return (num * factorial(num - 1));
+  }
 };
 
 // Do not edit below this line
