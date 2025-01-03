61bfdf65de9c78c941da9218b584ef8a760da20e
teguz2000 <189381830+teguz2000@users.noreply.github.com>
2025-01-03 12:07:17 +0100

    Done # Exercise 12 - Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    return people.reduce((result, person) => {
+
+        const yearsLivedPerson = (person.yearOfDeath || new Date().getFullYear()) - (person.yearOfBirth || new Date().getFullYear());
+        const yearsLivedResult = (result.yearOfDeath || new Date().getFullYear()) - (result.yearOfBirth || new Date().getFullYear());
+
+        if(yearsLivedPerson > yearsLivedResult)
+            result = person;
+
+        return result;
+    })
 };
 
 // Do not edit below this line
